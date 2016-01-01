80e3ae9a203995a96a0cb4ddea92314247693c54
nazunamoe <jpg3927@gmail.com>
2016-01-01 19:46:11 +0900

    fix build error

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -662,9 +662,9 @@ static int dump_rules(struct sk_buff *skb, struct netlink_callback *cb,
 			goto skip;
 
 		err = fib_nl_fill_rule(skb, rule, NETLINK_CB(cb->skb).portid,
-				       cb->nlh->nlmsg_seq, RTM_NEWRULE,
+				       cb->nlh->nlmsg_seq, RTM_NEWRULE, NLM_F_MULTI, ops);
 
-		if (err)
+		if (err<0)
 
 			break;
 skip:
